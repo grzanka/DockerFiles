8098465d98483e6147d025f3a50a606e1b6639ca
Leszek Grzanka <grzanka@agh.edu.pl>
2016-04-12 18:07:01 +0200

    CMSSW 810pre2 attempt

--- cmssw/manifest.pp
+++ cmssw/manifest.pp
@@ -16,35 +16,13 @@ file {"/opt/cms":
   ensure => directory,
   owner => "cmsbuild",
 }->
-package {"lcg+SCRAMV1+V2_2_6_pre5":
+package {"cms+cmssw+CMSSW_8_1_0_pre2":
   ensure             => present,
   provider           => cmsdist,
   install_options    => [{
     "install_prefix" => "/opt/cms",
     "install_user"   => "cmsbuild",
-    "architecture"   => "slc6_amd64_gcc493",
-    "server" => "cmsrep.cern.ch",
-    "server_path" => "cmssw/cms",
-  }]
-}->
-package {"cms+local-cern-siteconf+sm111124":
-  ensure             => present,
-  provider           => cmsdist,
-  install_options    => [{
-    "install_prefix" => "/opt/cms",
-    "install_user"   => "cmsbuild",
-    "architecture"   => "slc6_amd64_gcc493",
-    "server"         => "cmsrep.cern.ch",
-    "server_path"    => "cmssw/cms",
-  }]
-}->
-package {"cms+cmssw+CMSSW_8_1_0_pre1":
-  ensure             => present,
-  provider           => cmsdist,
-  install_options    => [{
-    "install_prefix" => "/opt/cms",
-    "install_user"   => "cmsbuild",
-    "architecture"   => "slc6_amd64_gcc493",
+    "architecture"   => "slc6_amd64_gcc530",
     "server"         => "cmsrep.cern.ch",
     "server_path"    => "cmssw/cms",
   }]
